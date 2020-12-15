ef7f6c4a08717a47dbce9ea6066b78366225a250
Pranabanandajana1990 <sonujana33@gmail.com>
2020-12-15 20:29:37 +0530

    shop page initiated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
+
 import Homepage from "./pages/homepage/homepage.component";
+
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
