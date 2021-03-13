5a449bc6b7d360ea08f6e60134f7d3f48c6bc115
Pranabanandajana1990 <sonujana33@gmail.com>
2021-03-13 11:56:28 +0530

    updating stripe integration and updating facicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishablekey =
+    "pk_test_51ISM8tLrxBNWxyi5y86XpucVQbjOflMZvYE6C6vXT4QnLZP2Q7jSvLaMw7J7ifvpXJ3x4ujxXtYtC1ixEbZdZJfL004PN0VQom";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishablekey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
